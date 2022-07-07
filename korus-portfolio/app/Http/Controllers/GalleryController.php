e8f9d562a8dc9c7dc3d447bbfa6f1a7fbbde48fd
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-07-07 09:33:26 +0200

    Database seeds for the News page

--- korus-portfolio/app/Http/Controllers/GalleryController.php
+++ korus-portfolio/app/Http/Controllers/GalleryController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
-    //
+    public function listGallery()
+    {
+        $galleries = Gallery::orderBy('id','desc')->paginate(50);
+
+        $galleries = Gallery::orderBy('id','desc')->paginate(50);
+        for($i=0;$i<sizeof($galleries);$i++){
+            $p_count = DB::table('pictures')
+                    ->select('gallery_id')
+                    ->where('gallery_id','=',$galleries[$i]['attributes']['id'])
+                    ->count();
+            $p_lead = DB::table('pictures')
+                    ->select('thumbnail')
+                    ->where('gallery_id','=',$galleries[$i]['attributes']['id'])
+                    ->first();
+            $copy = $galleries[$i]['attributes'];
+            $copy['p_count'] = $p_count;
+            $copy['p_lead'] = $p_lead;
+            $galleries[$i]['attributes'] = $copy;
+        }
+        $this->layout->main = View::make('home')->nest('content','galeria',compact('galleries'));
+    }
 }
