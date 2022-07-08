16ad29e3e2c4f66b78109368710b379b0ec382d3
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-07-08 11:58:11 +0200

    Redesign frontend in order to fix issues

--- korus-portfolio/resources/views/master.blade.php
+++ korus-portfolio/resources/views/master.blade.php
@@ -8,7 +8,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/style.css" rel="stylesheet">
-    <link href="/css/ystie.css" rel="stylesheet">
     <link href="//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.css" type="text/css" rel="stylesheet" />
     <script src="//code.jquery.com/jquery-latest.js"></script>
     <script src="//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
@@ -19,54 +18,63 @@
     <title>Egri Érseki Fiúkórus</title>
 </head>
 <body>
-    <div class="header">
-
-    <div class="container-fluid">
-        <div class="row">
-          <div class="col p-3">
-            <div style="height: 90px" class="container-fluid d-flex justify-content-center">
-                <div style="width: 144px; margin: 0">
+    <div class="oldal">
+        <div class="fejlec">
+            <div class="logo_box">
+                <div id="logo">
                     <a href="{{url('/')}}"><img src="{{URL::asset('/images/assets/logo_terra.png')}}" alt="logo" height="100"></a>
                 </div>
             </div>
-          </div>
+            <div class="menu">
+                <ul>
+                    <li class="menupont">
+                        <a href="{{url('/rolunk')}}">A KÓRUS</a>
+                    </li>
+                    <li class="menupont">
+                        <a href="{{url('/hirfolyam')}}">HÍRFOLYAM</a>
+                    </li>
+                    <li class="menupont">
+                        <a href="{{url('/galeria')}}">GALÉRIA</a>
+                    </li>
+                    <li class="menupont">
+                        <a href="#">ESEMÉNYNAPTÁR</a>
+                    </li>
+                    <li class="menupont">
+                        <a href="{{url('/kapcsolat')}}">KAPCSOLAT</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
-
-    <nav class="navbar navbar-expand-sm col" style="padding: 10px">
-
-
-        <div class="container-fluid d-flex justify-content-center">
-          <!-- Links -->
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link menupont" href="{{url('/rolunk')}}">A KÓRUS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link menupont" href="{{url('/hirfolyam')}}">HÍRFOLYAM</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link menupont" href="{{url('/galeria')}}">GALÉRIA</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link menupont" href="#">ESEMÉNYNAPTÁR</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link menupont" href="{{url('/kapcsolat')}}">KAPCSOLAT</a>
-              </li>
-            <li class="nav-item">
-                <a class="nav-link menupont" href="{{url('/kapcsolat')}}">BEJELENTKEZÉS</a>
-              </li>
-          </ul>
+        <div class="tartalom">
+          @yield('content')
         </div>
 
-      </nav>
+        <div class="clear"></div>
 
-    </div>
-    <div class="tartalom">
-      @yield('content')
-    </div>
+        <div class="sziluett" style="text-align: end">
+            <img src="{{URL::asset('/images/assets/sziluett_eger.png')}}">
+        </div>
 
-    @include('pages.footer')
+        <div class="lablec">
+            <div class="sitemap">
+                    <a href="{{url('/rolunk')}}">A kórus </a>
+                    /
+                    <a href="{{url('/hirfolyam')}}">Hírfolyam </a>
+                    /
+                    <a href="{{url('/galeria')}}">Galéria </a>
+                    /
+                    <a href="{{url('#')}}">Eseménynaptár </a>
+                    /
+                    <a href="{{url('/kapcsolat')}}">Kapcsolat</a>
+            </div>
+            <div class="footer_messages">
+                <a href="https://www.facebook.com/egriersekifiukorus"><div class="footer_facebook"></div></a>
+                <a href="mailto:egerfiukorus@gmail.com"><div class="footer_email"></div></a>
+            </div>
+            <div class="created">
+                Készítette: <a href="http://www.qnszt.hu"><span>QNSZT</span> Kft</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
