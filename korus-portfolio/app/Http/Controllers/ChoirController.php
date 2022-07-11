9835172ccfbb818174fadf394afb35ede067b3f4
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-07-11 12:05:26 +0200

    Implement store Current member function

--- korus-portfolio/app/Http/Controllers/ChoirController.php
+++ korus-portfolio/app/Http/Controllers/ChoirController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Models\Member;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Response;
+use Image;
+
 
 class ChoirController extends Controller
 {
@@ -31,5 +34,34 @@ class ChoirController extends Controller
         unlink($path. $pic);
         Member::destroy($member->id);
         return redirect('tag/current/edit');
+    }
+
+    public function storeCurrent(Request $request)
+    {
+        if($request->file('file')){
+
+            $file = $request->file('file');
+            $destinationPath = public_path().'/uploadfolder/members';
+            $filename = str::random(6) . '_' .($file->getClientOriginalName());
+            $uploadSuccess   = $file->move($destinationPath, $filename);
+
+            $image_th = Image::make($destinationPath.'/'.$filename);
+            if($image_th->width() > $image_th->height()){
+                $image_th->crop($image_th->height(),$image_th->height());
+            }else{
+                $image_th->crop($image_th->width(),$image_th->width());
+            }
+            $image_th->widen(124);
+            $image_th->save(null, 90);
+
+            $member = Member::create([
+                'source' => $filename,
+                'name' => $request->input('name'),
+                'is_old' => 0
+            ]);
+            return redirect('/tag/current/edit');
+        }else{
+            return redirect()->back('success', 'Nem volt file kiválasztva!');
+        }
     }
 }
