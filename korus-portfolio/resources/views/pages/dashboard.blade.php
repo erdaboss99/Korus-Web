c1c7adc0becbf66e97cbfc9fd4361d678353ac35
bPet75 <bpeti437@gmail.com>
2022-07-11 13:57:57 +0200

    Revert "Dashboard frontend, login frontend"
    This reverts commit a44f067d2dd249edb56d188a7ea1237c9304ccf3.

--- korus-portfolio/resources/views/pages/dashboard.blade.php
+++ korus-portfolio/resources/views/pages/dashboard.blade.php
@@ -24,18 +24,41 @@
         </div>
 	</div>
 
-    <form action="{{url('/')}}" method="POST" id="tag_newpass_form">
+    <form action="{{url('/')}}" method="POST">
         @csrf
+        <div class="d-flex">
+            <div class="d-flex mx-2">
                 <label>Új tag jelszó</label>
-            <div class="pass_labels">
+            </div>
+            <div class="d-flex mx-2 pass_labels">
                 <input type="password" id="tag_newpass" class="password2">
             </div>
-            <div class="pass_labels">
+            <div class="d-flex mx-2 pass_labels">
                 <label>Jelszó újra</label>
             </div>
+            <div class="d-flex mx-2">
                 <input type="password" id="tag_newpass2" class="password2">
-                <input type="submit" value="Változtatás">
+            </div>
+        </div>
     </form>
+
+	{{--{{ Form::open(array('url' => 'dash', 'id' => 'tag_newpass_form')) }}
+	{{ Form::label('tag_newpass', 'Új tag jelszó',
+                        array('class' => 'pass_labels')) }}
+	{{ Form::password('password', array('id' => 'tag_newpass')) }}
+	{{ Form::label('tag_newpass2', 'Jelszó újra',
+                        array('class' => 'pass_labels')) }}
+	{{ Form::password('password2', array('id' => 'tag_newpass2')) }}
+	{{ Form::submit('Változtatás') }}
+	{{ Form::close() }}
+	@if($errors->has())
+	@foreach ($errors->all() as $message)
+	<br><span class="warning">{{$message}}</span><br>
+	@endforeach
+	@endif
+	@if(Session::has('result'))
+	<br><span class="warning">{{Session::get('result')}}</span>
+	@endif--}}
 </div>
 
 @endsection
