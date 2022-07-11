2f070fe8cfb2e1eb9a997553ab6f150a26048e37
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-07-11 11:52:48 +0200

    Implement delete Current member function

--- korus-portfolio/app/Http/Controllers/ChoirController.php
+++ korus-portfolio/app/Http/Controllers/ChoirController.php
@@ -15,4 +15,21 @@ class ChoirController extends Controller
         $old_members = Member::all()->where('is_old', 1);
         return view('pages/aboutus', compact('current_members', 'old_members'));
     }
+
+    public function editCurrent(){
+        $members = Member::all()->where('is_old', 0);
+        return view('pages/member/current/edit', compact('members'));
+
+    }
+
+    public function deleteCurrent($id)
+    {
+        $member = Member::find($id);
+
+        $path = public_path('uploadfolder/members/');
+        $pic = $member->source;
+        unlink($path. $pic);
+        Member::destroy($member->id);
+        return redirect('tag/current/edit');
+    }
 }
